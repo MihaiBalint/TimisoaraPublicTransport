accdd7b3256470e9cd0d8d3dedb9b92182bf8030
Mihai Balint <mihai@cs.upt.ro>
2013-11-18 23:02:01 +0200

    start some api work

---
+++ Core-data/src/tpt/api.py
@@ -0,0 +1,31 @@
+
+from flask import Flask, url_for, request, jsonify
+app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Not Found: ' + request.url,
+        }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
+
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+
+@app.route('/generate_device_id')
+def generate_device_id():
+
+    return 'NONE\n'
+
+
+@app.route('/post_times_bundle')
+def post_times_bundle():
+
+    return 'Thank you\n'
