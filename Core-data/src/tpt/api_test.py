72da165ee2f0609e091df503de6782fcd24fe7bc
Mihai Balint <mihai@cs.upt.ro>
2013-11-24 02:25:17 +0200

    fix tests

--- Core-data/src/tpt/api_test.py
+++ Core-data/src/tpt/api_test.py
@@ -12,11 +12,13 @@ import tpt.db_test
 class DeviceIdGeneration(tpt.db_test.DatabaseSetup, unittest.TestCase):
 
     def test_generate_free_device_id(self):
+        tpt.db.create_database(self.conn)
         with contextlib.closing(self.conn.cursor()) as cursor:
             tpt.api.generate_device_id(cursor, used=False)
             self.assertNotNone(tpt.db.use_free_device_hash(cursor))
 
     def test_generate_used_device_id(self):
+        tpt.db.create_database(self.conn)
         with contextlib.closing(self.conn.cursor()) as cursor:
             tpt.api.generate_device_id(cursor, used=True)
             self.assertIsNone(tpt.db.use_free_device_hash(cursor))
