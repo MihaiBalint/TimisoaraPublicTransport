760164a4290dd22b4b8ec20022d0e5d7df71c578
Mihai Balint <mihai@cs.upt.ro>
2014-11-02 01:42:26 +0200

    Add/Update license headers to java files

--- Core-lib/src/ro/mihai/tpt/model/Path.java
+++ Core-lib/src/ro/mihai/tpt/model/Path.java
@@ -1,6 +1,6 @@
 /*
     TimisoaraPublicTransport - display public transport information on your device
-    Copyright (C) 2011-2013  Mihai Balint
+    Copyright (C) 2011-2014  Mihai Balint
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
