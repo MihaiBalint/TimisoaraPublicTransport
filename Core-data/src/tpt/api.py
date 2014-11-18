a673d918284404346e9718af16ea09508e8b597d
Mihai Balint <mihai@cs.upt.ro>
2014-11-19 01:24:17 +0200

    wip: use contextmanagers instead of try-catches to handle common errors

--- Core-data/src/tpt/api.py
+++ Core-data/src/tpt/api.py
@@ -25,6 +25,36 @@ def not_found_response(extras={}):
     return build_response("error", "Not Found: " + request.url, 404, extras)
 
 
+def exception_response(extras={}):
+    return build_response("error", "Internal server error", 500, extras)
+
+
+@contextlib.contextmanager
+def create_db_cursor():
+    try:
+        conn = tpt.db.open_connection()
+        cursor = conn.cursor()
+        try:
+            yield cursor
+            conn.commit()
+        except:
+            conn.rollback()
+            raise
+        finally:
+            cursor.close()
+            conn.close()
+    except Exception:
+        app.logger.exception("Error at {0}".format(request.url))
+
+
+@contextlib.contextmanager
+def not_found_handler():
+    try:
+        yield None
+    except tpt.db.ItemNotFoundException, e:
+        app.logger.info(e.message)
+
+
 @app.errorhandler(404)
 def not_found(error=None):
     err = {"error_code": error.code} if error is not None else {}
@@ -38,43 +68,20 @@ def do_api_root():
 
 @app.route('/generate_device_id', methods=["GET", "POST"])
 def do_generate_device_id():
-    try:
-        conn = tpt.db.open_connection()
-        try:
-            with contextlib.closing(conn.cursor()) as cursor:
-                device_id = tpt.tools.use_device_id(cursor)
-                conn.commit()
-                return device_id
-        except:
-            conn.rollback()
-            raise
-        finally:
-            conn.close()
-    except Exception:
-        app.logger.exception("Error getting unused device id.")
-        return 'NONE\n'
+    with create_db_cursor() as cursor:
+        device_id = tpt.tools.use_device_id(cursor)
+        return device_id
+    return 'NONE\n'
 
 
 @app.route('/post_times_bundle', methods=["POST", "GET"])
 def do_post_times_bundle():
-    try:
-        conn = tpt.db.open_connection()
-        try:
-            with contextlib.closing(conn.cursor()) as cursor:
-                tpt.tools.insert_times_log(cursor, request.stream)
-        except:
-            conn.rollback()
-            raise
-        finally:
-            conn.close()
-    except tpt.db.ItemNotFoundException, e:
-        app.logger.info(e.message)
+    with create_db_cursor() as cursor:
+        with not_found_handler():
+            tpt.tools.insert_times_log(cursor, request.stream)
+            return 'Thank you\n'
         return not_found()
-    except Exception:
-        app.logger.exception("Error saving times bundle.")
-        return 'Not saved, thank you anyway\n'
-    else:
-        return 'Thank you\n'
+    return 'Not saved, thank you anyway\n'
 
 
 def _get_fields():
@@ -99,7 +106,8 @@ def _map_routes(cursor, fields, routes):
         item_dict.update(route[3])
         item_dict.update(route[4])
 
-        stations = tpt.db.find_route_stations(cursor, route[0])
+        stations = tpt.db.find_route_stations(cursor, route[0], (
+            "route_stop_id", "stop_index", "stop_id"))
         # TODO find better way for getting a departure name
         item_dict.update({
             "route_id": route[0],
@@ -131,21 +139,14 @@ def _map_route_stops(cursor, fields, stops):
 
 
 def _do_any_routes(route_gen):
-    conn = tpt.db.open_connection()
-    try:
-        with contextlib.closing(conn.cursor()) as cursor:
+    with create_db_cursor() as cursor:
+        with not_found_handler():
             # TODO: paginate routes
             routes = list(_map_routes(
-                cursor, _get_fields(), **route_gen(cursor)))
+                cursor, _get_fields(), route_gen(cursor)))
             return build_response(extras={"routes": routes})
-    except tpt.db.ItemNotFoundException, e:
-        app.logger.info(e.message)
         return not_found_response({"routes": []})
-    except:
-        # TODO log exception
-        raise
-    finally:
-        conn.close()
+    return exception_response({"routes": []})
 
 
 def _mock_route_stops(fields):
@@ -206,27 +207,20 @@ def do_favorite_routes(city_id):
 
 @app.route('/v1/routes/<route_id>/stops', methods=["GET"])
 def do_route_stops(route_id):
-    conn = tpt.db.open_connection()
-    try:
-        with contextlib.closing(conn.cursor()) as cursor:
+    with create_db_cursor() as cursor:
+        with not_found_handler():
             # TODO: paginate stops
             stops = list(_map_route_stops(
                 cursor, _get_fields(),
                 tpt.db.find_route_stations(cursor, route_id, _get_fields())))
             return build_response(extras={"stops": stops})
-    except tpt.db.ItemNotFoundException, e:
-        app.logger.info(e.message)
         return not_found_response({"stops": []})
-    except:
-        # TODO log exception
-        raise
-    finally:
-        conn.close()
+    return exception_response({"stops": []})
 
 
 @app.route('/v1/routes/<route_id>/eta', methods=["GET"])
 def get_route_eta(route_id):
-    return build_response("error", "Not implemented")
+    return build_response("error", "Not implemented", 500)
 
 
 @app.route('/v1/eta/<route_ext_id>/<stop_ext_id>', methods=["GET"])
