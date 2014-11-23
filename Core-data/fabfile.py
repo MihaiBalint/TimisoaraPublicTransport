99fa666ecbb47bf782405431d5a91b658914a0d4
Mihai Balint <mihai@cs.upt.ro>
2014-11-23 23:37:12 +0200

    read token from config fileOF

--- Core-data/fabfile.py
+++ Core-data/fabfile.py
@@ -6,6 +6,7 @@ from fabric.context_managers import settings
 from termcolor import colored
 
 import digitalocean
+import json
 import os
 import sys
 
@@ -36,6 +37,22 @@ def test():
     local("nosetests -c .noserc --with-coverage")
 
 
+def _get_manager_server_map():
+    with open(".digitalocean.json", "rb") as config_file:
+        config = json.loads(config_file.read())
+
+    token = os.environ.get("DO_AUTH_TOKEN", config.get("auth_token"))
+    domg = digitalocean.Manager(token=token)
+    server_map = dict((dp.name, dp) for dp in domg.get_all_droplets())
+    return domg, server_map
+
+
+def _get_server(server_name):
+    domg, server_map = _get_manager_server_map()
+    return server_map[server_name] if server_name in server_map else \
+        domg.get_droplet(name=server_name)
+
+
 def deploy_test():
     error("Not implemented")
 
@@ -46,20 +63,14 @@ def deploy_live():
 
 def servers():
     info("Servers:")
-    token = os.environ.get("DO_TOKEN")
-    domg = digitalocean.Manager(token=token)
+    domg, _ = _get_manager_server_map()
     for dodp in domg.get_all_droplets():
         info("\t{0}\t{1}\t{2}\t{3}".format(
             dodp.id, dodp.ip_address, dodp.name, dodp.status))
 
 
 def poweron(server_name):
-    token = os.environ.get("DO_TOKEN")
-    domg = digitalocean.Manager(token=token)
-    server_map = dict((dp.name, dp) for dp in domg.get_all_droplets())
-
-    dodp = server_map[server_name] if server_name in server_map else \
-           domg.get_droplet(name=server_name)
+    dodp = _get_server(server_name)
     dodp.power_on()
     info("[{1}]: {0}".format(dodp.name, dodp.ip_address))
 
@@ -69,11 +80,6 @@ def _poweroff():
 
 
 def poweroff(server_name):
-    token = os.environ.get("DO_TOKEN")
-    domg = digitalocean.Manager(token=token)
-    server_map = dict((dp.name, dp) for dp in domg.get_all_droplets())
-
-    dodp = server_map[server_name] if server_name in server_map else \
-           domg.get_droplet(name=server_name)
+    dodp = _get_server(server_name)
     with settings(password="x", connection_attempts=30):
         execute(_poweroff, hosts=[dodp.ip_address])
