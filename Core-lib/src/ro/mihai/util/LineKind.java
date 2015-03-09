50de11330015753360d54b30d781b47298d0ccf3
Mihai Balint <mihai@cs.upt.ro>
2015-03-09 23:50:55 +0200

    update route data, app in beta

--- Core-lib/src/ro/mihai/util/LineKind.java
+++ Core-lib/src/ro/mihai/util/LineKind.java
@@ -22,7 +22,7 @@ import java.util.Iterator;
 import ro.mihai.tpt.model.Line;
 
 public enum LineKind {
-	TRAM("Tv1","Tv2","Tv4","Tv5","Tv6","Tv7","Tv8","Tv9"), 
+	TRAM("Tv1","Tv2","Tv4","Tv5","Tv6","Tv7","Tv8","Tv9", "Tv10"), 
 	TROLLEY("Tb11","Tb14","Tb15","Tb16","Tb17","Tb18"), 
 	BUS("3","13","21","22","28","29","32","33","33b","40","46"),
 	EXPRESS("E1","E2","E3","E4","E4b","E6","E7", "E8"),
