441cd9214a6fcc0013e77c1cbbbd9c6af9794748
Mihai Balint <mihai@cs.upt.ro>
2015-12-03 00:48:09 +0200

    Start work on code to map cryptic stop names from the new source to existing cryptic stop names

--- Core-test/src/ro/mihai/tpt/test/RATTTest.java
+++ Core-test/src/ro/mihai/tpt/test/RATTTest.java
@@ -26,7 +26,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ro.mihai.tpt.JavaCityLoader;
 import ro.mihai.tpt.RATT;
+import ro.mihai.tpt.model.City;
+import ro.mihai.tpt.model.Line;
+import ro.mihai.tpt.model.Path;
+import ro.mihai.tpt.model.Station;
+import ro.mihai.tpt.util.TestPrefs;
 
 public class RATTTest {
 
@@ -40,8 +46,31 @@ public class RATTTest {
 
 	@Test
 	public void testGetLines() throws IOException {
-		Enumeration<RATT.Est> result = RATT.downloadTimes2(null, "Tv1", "1106");
+		City c = JavaCityLoader.loadCachedCityOrDownloadAndCache(new TestPrefs());
+		String lineName = "Tv1";
+
+		Enumeration<RATT.Est> result = RATT.downloadTimes2(null, lineName, "1106");
 		assertNotNull(result);
 		assertTrue(result.hasMoreElements());
+		
+		Line l = c.getLineByName(lineName);
+		Path path = null;
+		RATT.Est est = result.nextElement();
+		for (Path p: l.getPaths()) {
+			String from = p.getEstimateByPath(0).getStation().getName();
+			if (from.equals(est.pathFrom)) {
+				path = p;
+				break;
+			}
+		}
+		assertNotNull(path);
+
+		while (result.hasMoreElements()) {
+			RATT.Est e = result.nextElement();
+			Station s = path.getEstimateByPath(e.stopNo).getStation();
+			assertEquals(e.stopCrypticName, s.getName());
+			if (e.pathFrom != est.pathFrom)
+				break;
+		}
 	}
 }
